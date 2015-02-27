4fc7826586bb90bb355cdae4794384272f85f416
Brandon <btoney434@gmail.com>
2015-02-27 07:10:25 -0500

    Updated schema

--- schema/dataGen.py
+++ schema/dataGen.py
@@ -7,4 +7,9 @@ with open('users.csv') as f:
 		print "INSERT INTO user_info (user_id, first_name, last_name) VALUES (%d,'%s','%s');" % (id,name[0],name[1])	
 		id += 1
 
-
+with open('courses.csv') as f:
+	id = 50000
+	for line in f:
+		items = f.split(line)
+		print "INSERT INTO (subject, course, section, title) VALUES ('%s',%d,%d,'%s');" % (items[0],items[1],items[2],items[3])
+		id += 1
